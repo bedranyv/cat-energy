9902cde23ed35820fa87fc65b71f15b2d0474727
bedranyv <evgen.bedran@gmail.com>
2020-10-12 17:21:48 +0300

    Закончил стилизацию index.html desktop

--- js/example-slider.js
+++ js/example-slider.js
@@ -1,19 +1,25 @@
-let imgContainer = document.querySelector(".example__img-container--before");
-let btnBefore = document.querySelector(".example__slider--text-before");
-let btnAfter = document.querySelector(".example__slider--text-after");
-let grip = document.querySelector(".example__slider--grip");
+let imgContainer = document.querySelector(".slider__image-wrapper--before");
+let btnBefore = document.querySelector(".slider__controls-text--before");
+let btnAfter = document.querySelector(".slider__controls-text--after");
+let thumb = document.querySelector(".slider__controls-scale");
 
+
+thumb.addEventListener("click", function onclick(event) {
+	event.preventDefault();
+
+	imgContainer.style.width = "calc(100% - " + thumb.value + "%)";
+});
 
 btnBefore.onclick = function (evt) {
 	evt.preventDefault();
-	imgContainer.style.width = "0";
-	grip.style.marginLeft = "calc(0% - " + 17 + "px)";
+	imgContainer.style.width = "100%";
+	thumb.value = "0";
 };
 
 btnAfter.onclick = function (evt) {
 	evt.preventDefault();
-	imgContainer.style.width = "100%";
-	grip.style.marginLeft = "calc(100% - " + 17 + "px)";
+	imgContainer.style.width = "0";
+	thumb.value = "100";
 };
 
 
